b5861b4027d411871ec7fd08f8bae578881e4868
RimshakanwalArin <160338555+RimshakanwalArin@users.noreply.github.com>
2024-03-23 10:11:36 +0500

    Add files via upload
    it is  a basic code typescript

---
+++ main.js
@@ -0,0 +1,44 @@
+"use strict";
+//beginner_steps
+//hello world
+console.log('hello world');
+console.log("Rimsha kanwal");
+//deta type :string,number,boolean
+//inference
+let a = "123";
+let b = "abc";
+console.log(a, b);
+let c = 123;
+console.log(c);
+let d = true;
+let e = false;
+console.log(d);
+//annotation
+let f = "flower";
+let g = 55;
+let h = false;
+console.log(h);
+//conconatination
+let J = '1';
+let k = 8;
+let l = J + k;
+console.log("this is a sum " + J + '+' + k + '=' + l);
+//tampete literal
+let m = 2;
+let n = 3;
+let o = m + n;
+console.log(`this is ${m}+ ${n} sum =${o}`);
+// variable //it is a global scrp
+var p = "abc"; //declare
+p = '123'; // update
+p = "orange"; //again update
+p = "peach"; //redeclare
+console.log(p); // peach
+// let  // block scrp
+let q = "apple"; //declare
+q = "peach"; //update
+q = "Orange"; // redeclare
+console.log(q);
+//const  // block scrp
+const w = "abc"; // declare
+console.log(w);
---
+++ main.ts
@@ -0,0 +1,63 @@
+
+
+
+//beginner_steps
+//hello world
+console.log('hello world');
+
+console.log("Rimsha kanwal");
+
+//deta type :string,number,boolean
+
+//inference
+
+let a = "123";
+let b = "abc";
+console.log(a , b);
+
+let c = 123;
+console.log(c);
+
+let d = true;
+let e = false;
+console.log(d);
+
+//annotation
+let f: string="flower";
+let g: number=55;
+let h : boolean= false;
+console.log(h);
+
+
+//conconatination
+let J = '1';
+let k = 8
+let l = J + k;
+console.log("this is a sum "  + J + '+' + k + '=' + l);
+
+//tampete literal
+
+let m = 2;
+let n = 3;
+let o = m+n;
+console.log(`this is ${m}+ ${n} sum =${o}`);
+
+
+// variable //it is a global scrp
+var p = "abc";  //declare
+ p = '123';  // update
+ p = "orange";  //again update
+ p = "peach";  //redeclare
+ console.log(p); // peach
+ 
+ // let  // block scrp
+ 
+ let q = "apple"; //declare
+ q = "peach";  //update
+ q = "Orange"; // redeclare
+ console.log(q);
+ 
+ 
+ //const  // block scrp
+ const w = "abc"; // declare
+ console.log(w);
